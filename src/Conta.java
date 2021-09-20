aaf8b2d81a84bffe9fda2ef490319cd41bd91ee2
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-20 16:14:42 -0300

    Adicionados atributos, método construtor e métodos de depósito, saque e saldo na classe Conta

--- src/Conta.java
+++ src/Conta.java
@@ -1,3 +1,34 @@
 public class Conta {
 
+    //Atributos
+    double numeroDaConta;
+    String nomeDoTitular;
+    double saldo = 0;
+
+    //Método construtor
+    public Conta(double numeroDaConta, String nomeDoTitular){
+        this.numeroDaConta = numeroDaConta;
+        this.nomeDoTitular = nomeDoTitular;
+    }
+
+    //Métodos
+
+    public void saque(double valorDoSaque){
+        this.saldo -= valorDoSaque;
+    }
+
+    double valorDoDeposito = 0;
+    public void deposita(double valorDoDeposito){
+        this.saldo += valorDoDeposito;
+    }
+
+    public String extrato(){
+        return nomeDoTitular
+                + " "
+                + numeroDaConta
+                + " "
+                + saldo;
+    }
+
+
 }
